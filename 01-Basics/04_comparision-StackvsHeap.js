678f91ed32e2fc14280260576500345ed6aba416
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2023-12-22 05:18:56 +0530

    update: added comparison

--- 01-Basics/04_comparision-StackvsHeap.js
+++ 01-Basics/04_comparision-StackvsHeap.js
@@ -1,16 +1,3 @@
-
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
-
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
-
-console.log("2" === 2);
-
-// ************************************
-
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
